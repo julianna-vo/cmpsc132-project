ca9e25bda814280779d3dd6c6c9b47871b074189
juvo720 <juvo720@gmail.com>
2026-04-28 17:32:10 -0400

    Final touches

--- project.py
+++ project.py
@@ -5,7 +5,9 @@ import random
 def number_guessing_game():
 
     # Introduction statement
+    print("\n" + "="*50) 
     print("\n Hello and Welcome to the Number Guessing Game!")
+    print("\n" + "="*50)
 
     # Player gets to choose the difficulty mode
     print("\n Select a difficulty mode!")
@@ -42,55 +44,58 @@ def number_guessing_game():
     print(f"\nGuess a number between {low} and {high}!")
 
     while not correct_guess:
-            
-        if max_attempts != float('inf'): # Scenario if max_attempts aren't unlimited, keep track of remaining attempts for user
-                remaining_attempts = max_attempts - num_attempts
-                print(f"You have {remaining_attempts} tries left to guess!")
 
         try:
             user_guess = int(input("Guess the number: ")) # Have user input their guess
             num_attempts += 1 # Increment num_attempts by 1 
+            guess_history.append((num_attempts, user_guess))
 
         except ValueError:
             print("Your guess should be a valid digit!") # Ensure that user input only contains digits
+            continue 
 
-        if user_guess > target_number: # Feedback given if user's guess is too high from target number
-            print("Too High")
-                    
-
-        elif user_guess < target_number: # Feedback given if user's guess is too low from target number
-            print("Too Low")
-
-        if (user_guess > target_number) or (user_guess < target_number): # Give hints to whether target number is even or odd only if hints_on (EASY OR MEDIUM MODE)
-            if hints_on:
-                if target_number % 2 == 0:
-                    print("HINT: The target number is even...")
-
-                if target_number % 2 != 0:
-                    print("HINT: The target number is odd...")
-
-        else: # Scenario if user correctly guesses the target number correctly
+        # Check the Guess
+        # SCENARIO 1: guess matches target 
+        if user_guess == target_number: # Feedback given if user's guess is too high from target number
             print("\nCorrect!")
             print(f"Congratulations! You guessed the correct number in {num_attempts} attempts!")
             correct_guess = True
-            guess_history.append((num_attempts, user_guess)) # Keep track of user's guess history
 
-        if not correct_guess and (max_attempts - num_attempts) == 0:
-            print(f"You're out of attempts! Game over!")
-            break
+        else:
+            # SCENARIO 2: guess is lower than target
+            if user_guess < target_number: # Feedback given if user's guess is too low from target number
+                print("\n*** Too Low ***")
 
-        if not correct_guess:
-            guess_history.append((num_attempts, user_guess))
-                
+            # SCENARIO 3: guess is higher than target
+            else:
+                print("\n*** Too High ***")
+
+            if hints_on: # Only for EASY and MEDIUM mode
+                print("="*30)
+
+                if target_number % 2 == 0: # Case if num is even
+                    print("HINT: The target number is even...")
+
+                else: # Case if num is odd
+                    print("HINT: The target number is odd...")
+
+                print("="*30)
+            
+            if max_attempts != float('inf'): # Scenario if max_attempts aren't unlimited, keep track of remaining attempts for user (MEDIUM and HARD MODE)
+                remaining_attempts = int(max_attempts - num_attempts)
+
+                if remaining_attempts > 0:
+                    print(f"*** You have {remaining_attempts} tries left to guess! ***")
+
+                else:
+                    print(f"No more attempts left! Game over!\nThe correct number was {target_number}!")
+                    break
+
     # Display a history log of user's attempts
     print("Let's look at a summary of all of your attempts!")
     for attempt_num, guess in guess_history:
         print(f"Attempt {attempt_num}: {guess}")
-
-
-number_guessing_game()
-                
-            
+           
 """
 Extra implementations:
 - Give hints for user after each incorrect guess (states if target number is even or odd) if user selects EASY or MEDIUM MODE
@@ -99,3 +104,18 @@ Extra implementations:
 - Tell user how many attempts they have left after each guess
 - Implement a "play again" to reset the game again if the user wishes to 
 """ 
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+def main():
+
+    # Implement a "Play again" feature
+    while True:
+        number_guessing_game()
+
+        play_again_choice = input("\nWant to play again? YES or NO: ").strip().lower()
+
+        if play_again_choice != 'yes':
+            print("Thanks for playing! See you next time!")
+            break
+
+if __name__ == "__main__":
+    main()
